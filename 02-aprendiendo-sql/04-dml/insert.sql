819d20a91966250a443ccc8de4bdc7736b535d76
Antonio <antonicut@gmail.com>
2025-01-16 12:02:17 +0100

    Add - Insertar registros - insert into

--- 02-aprendiendo-sql/04-dml/insert.sql
+++ 02-aprendiendo-sql/04-dml/insert.sql
@@ -2,13 +2,18 @@
 --  **********  insert.sql  **********
 --  ********************--************
 
---  **********  Insertar Registros  **********
+--  **********  Insertar Nuevos Registros  **********
 
 USE master_php_blog;
 
 INSERT INTO usuarios VALUES (null, 'Antonio', 'Cutillas', 'antonicut@gmail.com', '111111', '16-01-2025');
 INSERT INTO usuarios VALUES (null, 'Victor', 'Robles', 'victor@gmail.com', '222222', '17-01-2025');
 INSERT INTO usuarios VALUES (null, 'Juan', 'Gil', 'juangil@gmail.com', '222222', '18-01-2025');
+
+
+--  **********  Insertar Filas solo con ciertas columnas  **********
+
+INSERT INTO usuarios(email, password) VALUES('admin@admin.com', '555555');
 
 
 
