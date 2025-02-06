414dfcf689a222705f41c451e82c9e6e0511798e
Antonio <antonicut@gmail.com>
2025-02-06 21:01:10 +0100

    Add - Consultas de agrupamiento - Condiciones HAVING

--- 02-aprendiendo-sql/05-agrupamiento/02-consultas-sql.sql
+++ 02-aprendiendo-sql/05-agrupamiento/02-consultas-sql.sql
@@ -13,3 +13,8 @@ SELECT titulo, categoria_id from entradas GROUP BY categoria_id;
 SELECT COUNT(titulo) AS 'Nº Entradas', categoria_id from entradas GROUP BY categoria_id;
 
 
+-- **********  Consultas de Agrupamiento con Condiciones  **********
+SELECT COUNT(titulo) AS 'Nº Entradas', categoria_id from entradas GROUP BY categoria_id HAVING COUNT(titulo) >= 4;
+SELECT COUNT(titulo) AS 'Nº Entradas', categoria_id from entradas GROUP BY categoria_id HAVING COUNT(titulo) >= 3;
+SELECT COUNT(titulo) AS 'Nº Entradas', categoria_id from entradas GROUP BY categoria_id HAVING COUNT(titulo) >= 2;
+
